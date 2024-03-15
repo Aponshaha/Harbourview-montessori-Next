af28f5219e137c8a589d42a321dedc30db8a1ce0
aponsaha <devapon77@gmail.com>
2024-03-15 22:04:30 +0900

    Programs updated

--- src/data/program-data.js
+++ src/data/program-data.js
@@ -14,20 +14,26 @@ import author_1 from "@assets/img/program/author-1.png";
 
 const program_data = [
   {
-    id:1,
-    title:'Settling',
-    img:program_1,
-    description:'To round out our weekend of celebra tions, we are holding our reunion.',
+    id: 1,
+    title: 'Children’s House',
+    subTitle: 'Full-day Program',
+    img: program_1,
+    subDescription: 'Full-day Program - In Montessori Schools, children are grouped according to developmental stages rather than being separated on the basis of age/grade.',
+    description: 'The youngest children in our school engage in practical life activities designed to reinforce care of self and care of environment. The activities support the development of motor control, hand-eye coordination, concentration, independence and a sense of order.',
+    midDesc: 'They engage in sensorial activities with materials such as the beautifully crafted geometric solids, coloured cylinders and colour tablets. They move through the process of reading and writing with the sandpaper letters, movable alphabet and a variety of materials which lead to independence in reading and writing.',
+    cDesc: 'Children are exposed to addition, subtraction, multiplication and division through concrete experiences with Montessori math materials. They engage in collaborative problem solving and independent exploration.',
+    dDesc: 'Outdoor exploration is an important part of our Children’s House experience. The children go on nature hunts in our wooded area. They visit an animal farm and pumpkin patch every year. They grow plants in the classroom. They participate in science experiments, baking and art.',
+    learnings: 'In the Montessori prepared environment, children have freedom of choice and movement within clearly defined limits. The consistency of these freedoms and limits allows children to feel a sense of security and comfort and to feel the importance of community.',
     icon: <ProgramIconOne />,
-    clrClass:'clr-1',
-    authorImg:author_1,
-    authorName:'Alexia Honix',
+    clrClass: 'clr-1',
+    authorImg: author_1,
+    authorName: 'Alexia Honix',
     price: 175.00,
-    delay:'.2s',
-    programs:[
+    delay: '.2s',
+    programs: [
       {
         id: 1,
-        title: '6-7 Yrs',
+        title: '1-6 Yrs',
         subTitle: 'age',
       },
       {
@@ -37,26 +43,64 @@ const program_data = [
       },
       {
         id: 3,
-        title: '3.30 Hrs',
+        title: '5.30 Hrs',
         subTitle: 'period',
+      }
+    ],
+    info: 'The children are exposed to a wide variety of materials and activities in several key areas of education ',
+    keyArea: [
+      {
+        id: 1,
+        value: 'Sensorial'
+      },
+      {
+        id: 2,
+        value: 'Practical life'
+      },
+      {
+        id: 3,
+        value: 'Language'
+      },
+      {
+        id: 4,
+        value: 'Physical and biological sciences'
+      },
+      {
+        id: 5,
+        value: 'Cultural subjects'
+      },
+      {
+        id: 5,
+        value: 'mathematics'
+      },
+      {
+        id: 5,
+        value: 'geography'
       }
     ]
   },
   {
-    id:2,
-    title:'Play Group',
-    img:program_2,
-    description:'We will magical transform the School Sports Centre into a game field.',
-    icon: <ProgramIconTwo />,
-    clrClass:'clr-2',
-    delay:'.3s',
-    authorImg:author_1,
-    authorName:'Alexia Honix',
-    price: 195.00,
-    programs:[
+    id: 2,
+    title: 'Children’s House',
+    subTitle: 'Extended Day Program',
+    img: program_1,
+    subDescription: 'Extended Day Program - In Montessori Schools, children are grouped according to developmental stages rather than being separated on the basis of age/grade.',
+    description: 'The youngest children in our school engage in practical life activities designed to reinforce care of self and care of environment. The activities support the development of motor control, hand-eye coordination, concentration, independence and a sense of order.',
+    midDesc: 'They engage in sensorial activities with materials such as the beautifully crafted geometric solids, coloured cylinders and colour tablets. They move through the process of reading and writing with the sandpaper letters, movable alphabet and a variety of materials which lead to independence in reading and writing.',
+    cDesc: 'Children are exposed to addition, subtraction, multiplication and division through concrete experiences with Montessori math materials. They engage in collaborative problem solving and independent exploration.',
+    dDesc: 'Outdoor exploration is an important part of our Children’s House experience. The children go on nature hunts in our wooded area. They visit an animal farm and pumpkin patch every year. They grow plants in the classroom. They participate in science experiments, baking and art.',
+    learnings: 'In the Montessori prepared environment, children have freedom of choice and movement within clearly defined limits. The consistency of these freedoms and limits allows children to feel a sense of security and comfort and to feel the importance of community.',
+    icon: <ProgramIconOne />,
+    clrClass: 'clr-1',
+    authorImg: author_1,
+    authorName: 'Alexia Honix',
+    price: 175.00,
+    age: '1-6 Yrs',
+    delay: '.2s',
+    programs: [
       {
         id: 1,
-        title: '3-4 Yrs',
+        title: '1-6 Yrs',
         subTitle: 'age',
       },
       {
@@ -66,84 +110,59 @@ const program_data = [
       },
       {
         id: 3,
-        title: '3.30 Hrs',
+        title: '5.30 Hrs',
         subTitle: 'period',
       }
-    ]
-  },
-  {
-    id:3,
-    title:'Junior Nursery',
-    img:program_3,
-    description:'Kindedo not only for all our dedicated 2023 reunion year groups program.',
-    icon: <ProgramIconThree />,
-    clrClass:'clr-3',
-    delay:'.4s',
-    authorImg:author_1,
-    authorName:'Alexia Honix',
-    price: 285.00,
-    programs:[
+    ],
+    info: 'The children are exposed to a wide variety of materials and activities in several key areas of education ',
+    keyArea: [
       {
         id: 1,
-        title: '3-4 Yrs',
-        subTitle: 'age',
+        value: 'Sensorial'
       },
       {
         id: 2,
-        title: '5 Days',
-        subTitle: 'weekly',
+        value: 'Practical life'
       },
       {
         id: 3,
-        title: '3.30 Hrs',
-        subTitle: 'period',
+        value: 'Language'
+      },
+      {
+        id: 4,
+        value: 'Physical and biological sciences'
+      },
+      {
+        id: 5,
+        value: 'Cultural subjects'
+      },
+      {
+        id: 5,
+        value: 'mathematics'
+      },
+      {
+        id: 5,
+        value: 'geography'
       }
     ]
   },
   {
-    id:4,
-    title:'Nursery',
-    img:program_4,
-    description:'With connection with local licensed child care providers and other local.',
-    icon: <ProgramIconFour />,
-    clrClass:'clr-4',
-    delay:'.5s',
-    authorImg:author_1,
-    authorName:'Alexia Honix',
-    price: 140.00,
-    programs:[
+    id: 3,
+    title: 'Elementary',
+    subTitle: '',
+    img: program_1,
+    subDescription: 'Harbourview Montessori’s elementary program is exceptional, encompassing the best that Montessori curriculum has to offer.',
+    description: 'In this way, we create little communities of learners who help one another to learn and grow. We operate an after school program for working parents and for parents who need a little extra time to get things done.',
+    icon: <ProgramIconOne />,
+    clrClass: 'clr-1',
+    authorImg: author_1,
+    authorName: 'Alexia Honix',
+    price: 175.00,
+    delay: '.2s',
+    programs: [
       {
         id: 1,
-        title: '3-4 Yrs',
-        subTitle: 'age',
-      },
-      {
-        id: 2,
-        title: '5 Days',
-        subTitle: 'weekly',
-      },
-      {
-        id: 3,
-        title: '4.30 Hrs',
-        subTitle: 'period',
-      }
-    ]
-  },
-  {
-    id:5,
-    title:'Junior Kg',
-    img:program_5,
-    description:'The kinder garten was developed in the nineteenth year.',
-    icon: <ProgramIconFive />,
-    clrClass:'clr-5',
-    delay:'.6s',
-    authorImg:author_1,
-    authorName:'Alexia Honix',
-    price: 150.00,
-    programs:[
-      {
-        id: 1,
-        title: '4-5 Yrs',
+        title: '7-11 Yrs',
         subTitle: 'age',
       },
       {
@@ -159,20 +178,22 @@ const program_data = [
     ]
   },
   {
-    id:6,
-    title:'Senior Kg',
-    img:program_6,
-    description:'Jean-Jacques Rousseau’s belief in the inherent goodness of children.',
-    icon: <ProgramIconSix />,
-    clrClass:'clr-6',
-    delay:'.7s',
-    authorImg:author_1,
-    authorName:'Alexia Honix',
-    price: 170.00,
-    programs:[
+    id: 4,
+    title: 'After School Program',
+    subTitle: '',
+    img: program_1,
+    subDescription: 'We aim to cater to working parents who require extended care hours for their children.',
+    description: 'In this way, we create little communities of learners who help one another to learn and grow. We operate an after school program for working parents and for parents who need a little extra time to get things done.',
+    icon: <ProgramIconOne />,
+    clrClass: 'clr-1',
+    authorImg: author_1,
+    authorName: 'Alexia Honix',
+    price: 175.00,
+    delay: '.2s',
+    programs: [
       {
         id: 1,
-        title: '5-6 Yrs',
+        title: 'Elementary',
         subTitle: 'age',
       },
       {
@@ -182,12 +203,13 @@ const program_data = [
       },
       {
         id: 3,
-        title: '6.30 Hrs',
+        title: '3 Hrs',
         subTitle: 'period',
       }
     ]
   },
-  
+
+
 ]
 
 export default program_data;
