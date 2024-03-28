9cce1b5c0f5ab11870fc97bf9e033b52aebc3fbf
aponsaha <devapon77@gmail.com>
2024-03-28 12:16:22 +0900

    programs img testing

--- src/data/menu-data.js
+++ src/data/menu-data.js
@@ -56,13 +56,13 @@ const menu_data = [
       {
         title: 'List 2', link: '#',
         submenus: [
-          { programImg: true, title: 'Elementary', link: '/programs', img: '/assets/img/school/programs/will.jpg', btn: 'View More' },
+          { programImg: true, title: 'Elementary', link: '/programs', img: '/assets/img/school/programs/oli.jpg', btn: 'View More' },
         ]
       },
       {
         title: 'List 3', link: '#',
         submenus: [
-          { programImg: true, title: 'After School Program', link: '/programs', img: '/assets/img/school/programs/sophia.jpg', btn: 'View More' },
+          { programImg: true, title: 'After School Program', link: '/programs', img: '/assets/img/school/programs/oli.jpg', btn: 'View More' },
         ]
       }
     ]
